ee1da2e2a25e875ab59441bed39d7c37182de89d
Harsh Singh Raghuvanshi <harshsingh2298@gmail.com>
2024-06-02 14:11:49 +0530

    Delete VehicleInfo directory

--- VehicleInfo/src/main/java/Harsh/Singh/VehicleInfo/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package Harsh.Singh.VehicleInfo.Repository;
-
-
-import Harsh.Singh.VehicleInfo.Domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User,String> {
-}
--- VehicleInfo/src/main/java/Harsh/Singh/VehicleInfo/Repository/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package Harsh.Singh.VehicleInfo.Repository;
-
-import Harsh.Singh.VehicleInfo.Domain.Vehicle;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface VehicleRepository extends MongoRepository<Vehicle,String> {
-
-}
