dba75989d1fde3ab64ce999fc8014b59bab9fa95
Harsh Singh Raghuvanshi <harshsingh2298@gmail.com>
2024-06-02 14:11:26 +0530

    Delete CustomerService directory

--- CustomerService/src/main/java/Harsh/Singh/CustomerService/Exception/UserExist.java
+++
@@ -1,8 +0,0 @@
-package Harsh.Singh.CustomerService.Exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code= HttpStatus.CONFLICT,reason = "user already exists")
-public class UserExist extends Exception{
-}
--- CustomerService/src/main/java/Harsh/Singh/CustomerService/Service/SecurityTokenGenrator.java
+++
@@ -1,9 +0,0 @@
-package Harsh.Singh.CustomerService.Service;
-
-import Harsh.Singh.CustomerService.Domain.Customer;
-
-import java.util.Map;
-
-public interface SecurityTokenGenrator {
-    Map<String,String> generateToken(Customer customer);
-}
